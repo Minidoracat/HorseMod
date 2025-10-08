bd4a482f499d01243a616084998c7cf16d8af30e
AlexVDefi <alexandervdefi@gmail.com>
2025-10-08 02:09:22 +0100

    The Big Push
    All the remaining stuff lol
    
    Gallop animation sync needs to be fixed
    Hitching needs to be fixed

---
+++ Contents/mods/HorseMod/42/media/lua/client/HorseMod/horse/HorseInteractions.lua
@@ -0,0 +1,22 @@
+local HorseUtils  = require("HorseMod/Utils")
+local HorseRiding = require("HorseMod/Riding")
+-- local HorseAttachments = require("HorseMod/HorseAttachments")
+
+local function doHorseInteractionMenu(context, player, animal)
+    if not animal or not HorseUtils.isHorse(animal) then return end
+    if HorseRiding.canMountHorse(player, animal) then
+        -- FIXME: currently we set this variable here because animations are still in testing
+        -- we should detect when a horse spawns and apply this immediately
+        animal:setVariable("isHorse", true)
+        context:addOption(getText("IGUI_HorseMod_MountHorse"),
+                          player, HorseRiding.mountHorse, animal)
+    end
+end
+
+local function onClickedAnimalForContext(playerNum, context, animals, test)
+    if test then return end
+    if not animals or #animals == 0 then return end
+    doHorseInteractionMenu(context, getSpecificPlayer(playerNum), animals[1])
+end
+
+Events.OnClickedAnimalForContext.Add(onClickedAnimalForContext)
